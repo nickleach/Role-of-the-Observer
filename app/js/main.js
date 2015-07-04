d973e5167d51fde30600701496d6fa9111d470a8
Nick Leach <nickleach22@gmail.com>
2015-07-04 12:36:38 -0500

    Added orbit

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,37 @@
-console.log('Angular Rocks', angular);
+;( function(){
+
+  'use strict';
+
+
+angular.module('App', ['ui.router'])
+
+.config(['$stateProvider', '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise(('/'));
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'js/templates/home.tpl.html',
+      controller: 'HomeCtrl'
+    })
+    .state('band', {
+      url:'/band',
+      templateUrl: 'js/members/members.tpl.html',
+      controller: 'MembersCtrl'
+    })
+    .state('albums', {
+      url:'/music',
+      templateUrl: 'js/album.tpl.html',
+      controller: 'AlbumCtrl'
+    });
+
+  }])
+
+.controller('HomeCtrl', ['$scope',
+  function ($scope) {
+ $(document).foundation();
+}]);
+
+}());
